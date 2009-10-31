6ae07c3a3092aa77036050e970773051f7ea6c5f
Ernesto Jiménez <ernesto.jimenez@negonation.com>
2009-10-31 01:20:31 +0100

    too much :)

--- twitter/stream/http.rb
+++ twitter/stream/http.rb
@@ -5,8 +5,6 @@ module Twitter
     class HTTP < Twitter::HTTP
       self.host = "stream.twitter.com"
       self.accept = :json
-      self.user = "testmine"
-      self.password = "12341234"
       self.base_path = "/1"
     end
   end
